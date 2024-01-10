86b8e3ba1fca34efcefe101d909684ff63fe7c72
Lucas Hertzog <291828H@curtin.edu.au>
2024-01-11 02:23:56 +0800

    consultancy

--- compile.R
+++ compile.R
@@ -1,56 +1,34 @@
 library(tinytex)
 lapply(list.files("R", full.names = TRUE), source)
 
-# Set the flags for including or excluding sections
-config <- TRUE
-begin <- TRUE
-end <- TRUE
-contact <- TRUE
+fixed <- c(
+  "begin",
+  "end",
+  "config"
+)
 
-education <- FALSE
-education_short <- TRUE
+twopg <- c(
+  "contact",
+  "education",
+  "work_experience",
+  "grants_awards",
+  "un_consult",
+  "publications",
+  "presentations",
+  "skills",
+  "teaching",
+  "projects"
+)
 
-work_experience <- TRUE
-work_experience_short <- FALSE
+full_section(fixed, TRUE)
+full_section(twopg, FALSE)
+# full_section("supervision", F)
+# full_section("policy", F)
+# full_section("admin", F)
+# full_section("academic_community", F)
+# full_section("capacity_building", F)
+# full_section("media", F)
+# full_section("skills", F)
+# full_section("referees", F)
 
-publications <- TRUE
-publications_short <- FALSE
-
-presentations <- TRUE
-presentations_short <- FALSE
-
-grants_awards <- TRUE
-grants_awards_short <- FALSE
-
-teaching <- TRUE
-teaching_short <- FALSE
-
-supervision <- TRUE
-supervision_short <- FALSE
-
-projects <- TRUE
-projects_short <- FALSE
-
-policy <- TRUE
-policy_short <- FALSE
-
-admin <- TRUE
-admin_short <- FALSE
-
-academic_community <- TRUE
-academic_community_short <- FALSE
-
-capacity_building <- TRUE
-capacity_building_short <- FALSE
-
-media <- TRUE
-media_short <- FALSE
-
-skills <- TRUE
-skills_short <- FALSE
-
-referees <- TRUE
-referees_short <- FALSE
-
-# Run the compilation function
 compile_cv()
