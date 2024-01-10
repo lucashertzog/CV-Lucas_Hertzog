b37c52331aa8e6754ec7d1bfa7e1577db89e4fe0
Lucas Hertzog <291828H@curtin.edu.au>
2024-01-10 17:34:31 +0800

    slight

--- compile.R
+++ compile.R
@@ -3,24 +3,30 @@ lapply(list.files("R", full.names = TRUE), source)
 
 # Set the flags for including or excluding sections
 config <- TRUE
-contact <- TRUE
 begin <- TRUE
+end <- TRUE
+contact <- TRUE
+
 education <- TRUE 
 work_experience <- TRUE
+
 publications <- TRUE
 presentations <- TRUE
 grants_awards <- TRUE
+
 teaching <- TRUE
 supervision <- TRUE
+
 projects <- TRUE
 policy <- TRUE
 admin <- TRUE
 academic_community <- TRUE
 capacity_building <- TRUE
 media <- TRUE
+
 skills <- TRUE
+
 referees <- TRUE
-end <- TRUE
 
 # Run the compilation function
 compile_cv()
