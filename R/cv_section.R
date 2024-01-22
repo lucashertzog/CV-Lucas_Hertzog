41403e9babcc01d73acfc26944d52db2dfdc7b59
Lucas Hertzog <291828H@curtin.edu.au>
2024-01-22 10:55:14 +0800

    section

--- R/cv_section.R
+++ R/cv_section.R
@@ -25,29 +25,3 @@ cv_section <- function(...) {
     }
   }
 }
-
-
-# # Define the constants
-# FULL <- "FULL"
-# SHORT <- "SHORT"
-# OMIT <- "OMIT"
-# 
-# cv_section <- function(...) {
-#   args <- list(...)
-#   for (section_name in names(args)) {
-#     include_option <- args[[section_name]]
-# 
-#     if (include_option == FULL) {
-#       assign(section_name, TRUE, envir = globalenv())
-#       assign(paste0(section_name, "_short"), FALSE, envir = globalenv())
-#     } else if (include_option == SHORT) {
-#       assign(section_name, FALSE, envir = globalenv())
-#       assign(paste0(section_name, "_short"), TRUE, envir = globalenv())
-#     } else if (include_option == OMIT) {
-#       assign(section_name, FALSE, envir = globalenv())
-#       assign(paste0(section_name, "_short"), FALSE, envir = globalenv())
-#     } else {
-#       stop("Invalid inclusion option for section '", section_name, "'. Options are FULL, SHORT, or OMIT.")
-#     }
-#   }
-# }
